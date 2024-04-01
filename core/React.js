8b2270268616a534fdb628579a4be97fb04e4bb0
github@hbw <1127621466@qq.com>
2024-04-01 17:40:05 +0800

    feat: 重构

--- core/React.js
+++ core/React.js
@@ -14,30 +14,141 @@ function createElement(type, props, ...children) {
     props: {
       ...props,
       children: children.map((child) => {
-        return typeof child === "string" ? createTextNode(child) : child;
+        const isTextNode =
+          typeof child === "string" || typeof child === "number";
+        return isTextNode ? createTextNode(child) : child;
       }),
     },
   };
 }
 
 function render(el, container) {
-  const dom =
-    el.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(el.type);
-  console.log(el);
-  Object.keys(el?.props).forEach((key) => {
+  nextWorkOfUnit = {
+    dom: container,
+    props: {
+      children: [el],
+    },
+  };
+  root = nextWorkOfUnit;
+}
+
+function createDom(type) {
+  return type === "TEXT_ELEMENT"
+    ? document.createTextNode("")
+    : document.createElement(type);
+}
+
+function updateProps(dom, props) {
+  Object.keys(props).forEach((key) => {
     if (key !== "children") {
-      dom[key] = el.props[key];
+      if (key.startsWith("on")) {
+        const eventType = key.slice(2).toLowerCase();
+        dom.addEventListener(eventType, props[key]);
+      } else {
+        dom[key] = props[key];
+      }
     }
   });
-  const children = el.props.children;
-  children.forEach((child) => {
-    // 此处是一个递归
-    render(child, dom);
-  });
-  container.append(dom);
 }
+
+function initChildren(fiber, children) {
+  let prevChild = null;
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      sibling: null,
+      dom: null,
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevChild.sibling = newFiber;
+    }
+    prevChild = newFiber;
+  });
+}
+
+let nextWorkOfUnit = null;
+let root = null;
+let currentRoot = null;
+function workLoop(deadLine) {
+  let shouldYied = false;
+
+  while (!shouldYied && nextWorkOfUnit) {
+    nextWorkOfUnit = performUnitOfWork(nextWorkOfUnit);
+    shouldYied = deadLine.timeRemaining() < 1;
+  }
+
+  if (!nextWorkOfUnit && root) {
+    commit();
+  }
+
+  requestIdleCallback(workLoop);
+}
+
+function commit() {
+  commitwork(root.child);
+  // 在提交之前，记录当前dom树的根节点
+  currentRoot = root;
+  root = null;
+}
+
+function commitwork(fiber) {
+  if (!fiber) return;
+  let fiberParent = fiber.parent;
+  while (!fiberParent.dom) {
+    fiberParent = fiberParent.parent;
+  }
+  if (fiber.dom) fiberParent.dom.append(fiber.dom);
+
+  commitwork(fiber.child);
+  commitwork(fiber.sibling);
+}
+
+function updateFunctionComponet(fiber) {
+  const children = [fiber.type(fiber.props)];
+  initChildren(fiber, children);
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type));
+    // work.parent.dom.append(dom);
+    // 处理props
+    updateProps(dom, fiber.props);
+  }
+  const children = fiber.props.children;
+  // 转换链表 设置指针
+  initChildren(fiber, children);
+}
+
+function performUnitOfWork(work) {
+  const isFunctionComponent = typeof work.type === "function";
+  if (!isFunctionComponent) {
+    updateHostComponent(work);
+  } else {
+    updateFunctionComponet(work);
+  }
+  // 返回下一个需要执行的任务
+  if (work.child) {
+    return work.child;
+  }
+  if (work.sibling) return work.sibling;
+  // 当前节点 即无子节点也无兄弟节点 就返回叔叔节点
+  let nextFiber = work;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+  // return work.parent.sibling;
+}
+
+requestIdleCallback(workLoop);
 
 const React = {
   render,
